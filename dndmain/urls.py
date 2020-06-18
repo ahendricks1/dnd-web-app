5456c8f09cd2c09aa6a61b33fa503936448be3af
Austin Hendricks <hendraus@oregonstate.edu>
2020-06-17 18:07:10 -0700

    Create more simple views for party and detailed character sheet

--- dndmain/urls.py
+++ dndmain/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('', views.index, name = 'index'),
+	path('<int:character_id>/detail/', views.detail, name = 'detail'),
+	path('<int:party_id>/party/', views.party, name = 'party'),
 ]
--- dndmain/views.py
+++ dndmain/views.py
@@ -2,3 +2,10 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello, world! You're on the DnD homepage.")
+
+def detail(request, character_id):
+	return HttpResponse("You're looking at character %s." % character_id)
+
+def party(request, party_id):
+	party = "You're looking at party #%s."
+	return HttpResponse(party % party_id)
