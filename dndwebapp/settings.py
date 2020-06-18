f77b95a5855b41147a118881667049e7cb0ac2ec
Austin Hendricks <hendraus@oregonstate.edu>
2020-06-17 17:12:18 -0700

    Add DndmainConfig to installed apps

--- dndwebapp/settings.py
+++ dndwebapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dndmain.apps.DndmainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
