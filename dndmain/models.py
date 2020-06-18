037f4caabd8bddefa3207f256caad978919beb18
Austin Hendricks <hendraus@oregonstate.edu>
2020-06-17 17:13:43 -0700

    Fix class syntax error

--- dndmain/models.py
+++ dndmain/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Party(models.Model)
+class Party(models.Model):
 	dungeon_master = models.CharField(max_length = 100)
 	creation_date = models.DateTimeField('date published')
 
-class Character(models.Model)
+class Character(models.Model):
 	party = models.ForeignKey(Party, on_delete = models.CASCADE)
 	real_name = models.CharField(max_length = 100)
 	char_name = models.CharField(max_length = 100)	
