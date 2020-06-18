8a55aca76a37227f71c3f8f0fe2e11ba83155aa9
Austin Hendricks <hendraus@oregonstate.edu>
2020-06-17 17:10:44 -0700

    Create party and character model for testing

--- dndmain/models.py
+++ dndmain/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Party(models.Model)
+	dungeon_master = models.CharField(max_length = 100)
+	creation_date = models.DateTimeField('date published')
+
+class Character(models.Model)
+	party = models.ForeignKey(Party, on_delete = models.CASCADE)
+	real_name = models.CharField(max_length = 100)
+	char_name = models.CharField(max_length = 100)	
