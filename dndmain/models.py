48c25b85914f605a577f8cde4bc37c75490faafa
Austin Hendricks <hendraus@oregonstate.edu>
2020-06-17 17:30:08 -0700

    Add str methods to both Party and Character for readability

--- dndmain/models.py
+++ dndmain/models.py
@@ -4,7 +4,16 @@ class Party(models.Model):
 	dungeon_master = models.CharField(max_length = 100)
 	creation_date = models.DateTimeField('date published')
 
+	def __str__(self):
+		return self.dungeon_master
+
+	def was_created_recently(self):
+		return self.creation_date >= timezone.now() - datetime.timedelta(days = 1)
+
 class Character(models.Model):
 	party = models.ForeignKey(Party, on_delete = models.CASCADE)
 	real_name = models.CharField(max_length = 100)
-	char_name = models.CharField(max_length = 100)	
+	char_name = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.char_name
