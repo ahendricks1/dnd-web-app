19c8764ad4ed7bfc0f22e05eb67ee159fdecaa9b
jkelley1991 <63363735+jkelley1991@users.noreply.github.com>
2020-06-27 10:50:52 -0500

    Update urls.py

--- dndwebapp/urls.py
+++ dndwebapp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-	path('dndmain/', include('dndmain.urls')),
+	path('', include('dndmain.urls')),
     path('admin/', admin.site.urls),
 ]
