92913bd9bb3f61a66742f187749724efa2fee2fa
Austin Hendricks <hendraus@oregonstate.edu>
2020-07-10 11:00:52 -0700

    Remove unused background

--- dndmain/views.py
+++ dndmain/views.py
@@ -49,10 +49,10 @@ def edit_character(request, character_id):
 
 @login_required
 def create(request, user_id):
-	#if not request.user.is_authenticated:
-	#	return render(request, 'dndmain/home.html')
-#
-	#else:
+	if not request.user.is_authenticated:
+		return render(request, 'dndmain/home.html')
+
+	else:
 		form = CharacterForm(request.POST or None)
 		if(form.is_valid()):
 			form = form.save()
