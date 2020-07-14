050a3606b7ea434396799e63d19da951abfc6db5
Austin Hendricks <hendraus@oregonstate.edu>
2020-07-14 09:36:15 -0700

    Give user the ability to delete their character

--- dndmain/urls.py
+++ dndmain/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 	path('party/<int:pk>/', views.party, name = 'party'),
 	path('character/<int:character_id>/', views.character, name = 'character'),
 	path('character/<int:character_id>/edit/', views.edit_character, name = 'edit_character'),
+	path('character/<int:character_id>/delete/', views.delete_character, name = 'delete_character'),
 	path('profile/<int:user_id>/', views.profile, name = 'profile'),
 	path('create/<int:user_id>/', views.create, name = 'create'),
 ]
